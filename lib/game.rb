5bd9707ea9ace853f202bb2a09f1788481aea7ae
Jordan Bailey <jordanbailey201@gmail.com>
2018-10-22 07:48:08 +0000

    basic RPSSL game finished moving on to the web setup

--- lib/game.rb
+++ lib/game.rb
@@ -1,11 +1,28 @@
 class Game
-    attr_reader :type, :player2, :cpu_hand
+    attr_reader :type, :player2, :cpu_hand, :winner
+  
+    RPS_RULES = { 'rock' => %w(scissors lizard), 
+                'scissors' => %w(paper lizard), 
+                'paper' => %w(rock spock), 
+                'lizard' => %w(paper spock), 
+                'spock' => %w(rock scissors) } 
+  
+  
+  
     def initialize(player1, type = 'RPS', player2 = 'CPU')
         @type = type
         game_type_ok?
+        @player1 = player1
         @player2 = player2
         @cpu_hand =cpu_plays if @player2 == 'CPU'
     end
+
+    def decision(player1, player2 = 'CPU') 
+      second_hand = (@player2 == 'CPU' ? @cpu_hand : @player2.hand) 
+      @winner = (player1.hand == second_hand ? 'draw' : player2) 
+      RPS_RULES.each { |wins, loses| @winner = player1 if player1.hand == wins && loses.include?(second_hand) } 
+    end 
+
 private
     def game_type_ok?
         fail 'You can only play a RPS type game' unless (@type == 'RPS' || @type == 'RPSSL')
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -7,8 +7,10 @@ require 'game'
 
  
 describe Game do 
-    let(:player1) { double:player }
+    let(:player1) { double:player, hand: 'rock' }
+    let(:player2) {double :player, hand: 'scissors' }
     let(:game) {Game.new(:player) }
+    let(:two_players_game) { Game.new(:player1, 'RPS', player2) } 
     it 'knows we are playing RPS if not mentions otherwise' do
         expect(game.type).to eq 'RPS'
     end
@@ -20,8 +22,7 @@ describe Game do
         expect { Game.new(:player1, 'battleships') }.to raise_error 'You can only play a RPS type game'
     end
     it 'can be player by 1 but also by 2 players' do
-     player2 = double(:player) 
-     expect { Game.new(:player1, 'RPS', player2) }.not_to raise_error 
+        expect { Game.new(:player1, 'RPS', player2) }.not_to raise_error 
     end
     it 'knows that if there is only one player online, the 2nd player is the CPU' do 
        expect(game.player2).to eq 'CPU' 
@@ -29,5 +30,38 @@ describe Game do
     it 'chooses a random hand if there is onlu one player online' do
         expect(game.cpu_hand).to satisfy {'rock' || 'paper' || 'scissors' }
     end
- 
+    it 'wont choose a hand if there is a second player online' do
+       expect(two_players_game.cpu_hand).to be nil
+    end
+    it 'knows when the game is a draw' do 
+      player_draw = double(:player, hand: 'rock') 
+      draw_game = Game.new(:player1, 'RPS', player_draw) 
+      draw_game.decision(player1, player_draw) 
+      expect(draw_game.winner).to eq 'draw' 
+    end 
+   it 'knows who the winner of a 2 players RPS game is' do
+     two_players_game.decision(player1, player2)
+     expect(two_players_game.winner).to eq player1
+     player3 = double(:player, hand: 'paper')
+     p3wins = Game.new(:player1, 'RPS', player3)
+     p3wins.decision(player1, player3)
+     expect(p3wins.winner).to eq player3
+   end
+  it 'knows who the winner of a 2 players RPSSL game is' do
+     player4 = double(:player, hand: 'lizard')
+     p1wins = Game.new(:player1, 'RPS', player4)
+     p1wins.decision(player1, player4)
+     expect(p1wins.winner).to eq player1
+     player5 = double(:player, hand: 'spock')
+     p5wins = Game.new(:player1, 'RPS', player5)
+     p5wins.decision(player1, player5)
+     expect(p5wins.winner).to eq player5
+   end
+    it 'knows who the winner of a 1 player vs CPU RPS game is' do
+     100.times do
+       new_game = Game.new(:player1)
+       new_game.decision(player1)
+       expect(new_game.winner).to satisfy { player1 || 'DRAW!' || 'CPU' }
+    end 
+  end 
 end 
